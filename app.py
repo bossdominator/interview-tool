105e921603d04c7591e1637d5ca358decbdfbb61
Vincent <mcwhyne@yahoo.com>
2026-09-09 20:42:22 +0800

    updated code

---
+++ app.py
@@ -0,0 +1,31 @@
+import streamlit as st
+from openai import OpenAI
+
+st.set_page_config(page_title = "Streamlit Chat", page_icon = "speech_balloon")
+st.title("Chatbot")
+
+client= OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
+
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-4o"
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+
+if prompt := st.chat_input ("Your answer"):
+    st.session_state.messages.append({"role ": "user", "content" :prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    with st.chat_message("assistant"):
+        stream = client.chat.completions.create(
+            model = st.session_state["openai_model"],
+            messages=[
+                {"role": m["role"], "content": m["content"]}
+                for m in st.session_state.messages
+            ],
+            stream = True
+        )
+        response = st.write_stream(stream)
+    st.session_state.messages.append({"role": "assistant", "content": response})
---
+++ example_1.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.title("Nested Buttons Example")
+
+if 'show_second_button' not in st.session_state:
+    st.session_state.show_second_button = False
+
+if st.button("First Button"):
+    st.session_state.show_second_button = True
+
+if st.session_state.show_second_button:
+    st.write("Revealed")
+    
+    if st.button("Seccond Button"):
+        st.write("Second button clicked")
+        st.write("Revealed")
